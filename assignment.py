d3226f82756f822a901efa6f7354b2e70c9438e0
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-11-08 13:34:39 -0800

    update addition part

--- assignment.py
+++ assignment.py
@@ -3,6 +3,7 @@
 # Feel free to rename your variables
 
 #addition, subtraction, multiplication, division, area, volume, square root, surface area.
+import math
 
 def title(symbol = '='):
     # Will display a title screen
@@ -14,7 +15,7 @@ def title(symbol = '='):
     output = f"{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}\n{symbol} Calculator {symbol}\n{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}{symbol}"
     return output
 
-#def instructions():
+def instructions():
     # Will display instructions
     # input parameters: none needed
     # output parameters: None
@@ -27,17 +28,22 @@ def title(symbol = '='):
     return output
     
 
-
-
-#def main():
+def main():
     """
     main block of code that will run your program and control program flow
     You will need to include a while loop to keep repeating the commands until
     the user chooses to exit
     """
+    type_of_calc = input("Choose what you want to calculate: ")
+    if type_of_calc == 'Addition' or type_of_calc == 'addition':
+        var_a = input('Enter a number: ')
+        var_b = input('Enter anumber to add to the first number: ')
+        sum = var_a + var_b
+        print(f"The sum of {var_a} and {var_b} is {sum}")
+
     decision = input("If you want to exit enter 'Exit' and if you want to stay enter 'Continue'")
     if decision == 'Continue' or decision == "continue":
-        title()
+        print(title())
         while True:
         # keep giving options to choose menu options until they choose to exit
             pass
@@ -46,5 +52,6 @@ def title(symbol = '='):
 
 if __name__ == "__main__":
     print(title())
+    print(instructions())
     print(main())
 
