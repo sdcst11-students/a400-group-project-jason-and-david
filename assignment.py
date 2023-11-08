00c3050ab4f59f22a92d4f9fe3ead4baeb614609
Jasonl1m <jasonl694@deltalearns.ca>
2023-11-08 13:11:33 -0800

    update

--- assignment.py
+++ assignment.py
@@ -2,36 +2,44 @@
 # Volume Calculator
 # Feel free to rename your variables
 
+#addition, subtraction, multiplication, division, area, volume, square root, surface area.
 
-def title():
+#def title():
     # Will display a title screen
     # input parameters: none needed
     # output parameters: None
     # Author: Copper
     # Modified:
     # title
-    return None
+   #return None
 
 def instructions():
     # Will display instructions
     # input parameters: none needed
     # output parameters: None
-    # Author:
+    # Author: Jason
     # Modified:
-    return None
+    output = print("---------------------------------------------------------------------------------------------")
+    print("| print out one of the calculations you choose to use =                                     |")
+    print("| addition, subtraction, multiplication, division, area, volume, square root, surface area. |")
+    print("---------------------------------------------------------------------------------------------")
+    return output
+    
 
 
 
-def main():
+#def main():
     """
     main block of code that will run your program and control program flow
     You will need to include a while loop to keep repeating the commands until
     the user chooses to exit
     """
-    title()
-    while True:
+    #title()
+    #while True:
         # keep giving options to choose menu options until they choose to exit
-        pass
+        #pass
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     main()
+
+instructions()
