914b3add17073205814ce8cf13a3ec0019016338
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-11-14 12:33:28 -0800

    update

--- assignment.py
+++ assignment.py
@@ -46,7 +46,7 @@ def main():
         print(f"The difference of {sub_a} and {sub_b} is {diff}.")
     elif type_of_calc == 'Multiplication' or type_of_calc == 'multiplication':
         mult_a = float(input('Enter a number: '))
-        mult_b = int(input('Enter a number to multiply the first number by: '))
+        mult_b = float(input('Enter a number to multiply the first number by: '))
         prod = mult_a * mult_b
         print(f"The product of {mult_a} and {mult_b} is {prod}.")
     elif type_of_calc == 'Division' or type_of_calc == 'division':
